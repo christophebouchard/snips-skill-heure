df580de6b97a2f66a092a3734e7728bd3f11a528
christophebouchard <christophe1.bouchard@orange.com>
2018-09-27 10:57:07 +0200

    Update action-heure.py

--- action-heure.py
+++ action-heure.py
@@ -94,7 +94,7 @@ def intent_received(hermes, intent_message):
 		
 		print('sponsor')
 		print(intent_message.intent.intent_name)
-		sentence = "Je sais qu'ils sont fatiguer, mais nous avons encore besoin d'eux, un dernier effort, toutouyoutou, toutouyoutou!"
+		sentence = "Je sais qu'ils sont fatiguer, mais nous avons encore besoin d'eux, un dernier effort, toutou you tou, toutou you tou!"
 		print(sentence)
 		hermes.publish_end_session(intent_message.session_id, sentence)
 		
